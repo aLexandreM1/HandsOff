b2df9b55070bdc9038b0d36d3a2302f2335c443d
Jao Pedro <jpfaria19@hotmail.com>
2018-09-26 19:51:01 -0300

    Refatorando código, limpando os problemas que o merge deixou

---
+++ app/src/main/java/com/android/projeto/handsoff/service/PhoneCallInterceptor.java
@@ -0,0 +1,68 @@
+package com.android.projeto.handsoff.service;
+
+import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.annotation.RequiresApi;
+import android.telephony.SmsManager;
+import android.telephony.TelephonyManager;
+import android.widget.Toast;
+
+
+import com.android.projeto.handsoff.ITelephony;
+
+import java.lang.reflect.Method;
+
+@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+public class PhoneCallInterceptor extends BroadcastReceiver {
+    private String getContactName(Context context, String phoneNumber) {
+        ContentResolver cr = context.getContentResolver();
+        Uri uri = Uri.withAppendedPath(ContactsContract.CommonDataKinds.Contactables.CONTENT_FILTER_URI, Uri.encode(phoneNumber));
+        Cursor cursor = cr.query(uri, new String[]{ContactsContract.PhoneLookup.DISPLAY_NAME}, null, null, null);
+
+        if (cursor.moveToFirst()) {
+            phoneNumber = cursor.getString(cursor.getColumnIndex(ContactsContract.PhoneLookup.DISPLAY_NAME));
+            cursor.close();
+            return phoneNumber;
+        } else {
+            cursor.close();
+            return null;
+        }
+    }
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+
+        try {
+            Method m = Class.forName(tm.getClass().getName()).getDeclaredMethod("getITelephony");
+            m.setAccessible(true);
+
+            ITelephony telephonyService = (ITelephony) m.invoke(tm);
+            telephonyService.silenceRinger();
+            telephonyService.endCall();
+
+            Bundle b = intent.getExtras();
+            String incomingNumber = b.getString(TelephonyManager.EXTRA_INCOMING_NUMBER);
+            String contactName = getContactName(context, incomingNumber);
+
+            StringBuilder auxAppender = new StringBuilder();
+            for (int i = 0; i < incomingNumber.length(); i++)
+                auxAppender.append(incomingNumber.charAt(i)).append(" ");
+
+            Toast.makeText(context, "Ligação de " + (contactName != null ? contactName : incomingNumber) + " rejeitada.", Toast.LENGTH_SHORT).show();
+            TTSApp.speak("Ligação de " + (contactName != null ? contactName : auxAppender.toString()) + " rejeitada.");
+
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(incomingNumber, null, "Estou ocupado. Por gentileza, retorne mais tarde. Grato.", null, null);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
---
+++ app/src/main/java/com/android/projeto/handsoff/service/SMSInterceptor.java
@@ -0,0 +1,50 @@
+package com.android.projeto.handsoff.service;
+
+import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.ContactsContract;
+import android.provider.Telephony;
+import android.support.annotation.RequiresApi;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
+
+@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+public class SMSInterceptor extends BroadcastReceiver {
+    private String getContactName(Context context, String phoneNumber) {
+        ContentResolver cr = context.getContentResolver();
+        Uri uri = Uri.withAppendedPath(ContactsContract.CommonDataKinds.Contactables.CONTENT_FILTER_URI, Uri.encode(phoneNumber));
+        Cursor cursor = cr.query(uri, new String[]{ContactsContract.PhoneLookup.DISPLAY_NAME}, null, null, null);
+
+        if (cursor.moveToFirst()) {
+            phoneNumber = cursor.getString(cursor.getColumnIndex(ContactsContract.PhoneLookup.DISPLAY_NAME));
+            cursor.close();
+            return phoneNumber;
+        } else {
+            cursor.close();
+            return null;
+        }
+    }
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        for (SmsMessage smsMessage : Telephony.Sms.Intents.getMessagesFromIntent(intent)) {
+            String messageBody = smsMessage.getMessageBody();
+            String phone = smsMessage.getOriginatingAddress();
+            String contactName = getContactName(context, phone);
+
+            //Separando números com espaços para que o TTS fale os números individualmente
+            StringBuilder auxAppender = new StringBuilder();
+            for (int i = 0; i < phone.length(); i++)
+                auxAppender.append(phone.charAt(i)).append(" ");
+
+            Toast.makeText(context, "Mensagem de " + (contactName != null ? contactName : phone), Toast.LENGTH_SHORT).show();
+            //TODO: resolver problema de textos com acentuação que estão sendo pulados
+            TTSApp.speak("MENSAGEM DE " + (contactName != null ? contactName : auxAppender.toString()) + ". MENSAGEM." + messageBody.toUpperCase());
+        }
+    }
+}
